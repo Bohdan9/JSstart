892dc0bfdbfc119f95c270254f583347af89d7b7
bohdan9 <protsbohdan9@gmail.com>
2019-05-30 18:50:13 +0300

    [ADD] create promise function and

--- demo.js
+++ demo.js
@@ -1,17 +1,10 @@
 let selectMonth = 0;
-let usersObject = getArrayByMonthAndYear();
-usersObject.sortedKeys = sortByYearAndMonth(usersObject.keySet);
+
 
 function getRandomEmail() {
     const domain = '@email.com';
     const rand = Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5);
     return rand + domain;
-}
-
-function getRandomID(min, max) {
-    let randomId = Math.random() * (max - min) + min;
-    randomId = Math.round(randomId);
-    return randomId;
 }
 
 function generateRandomDate(start, end) {
@@ -28,7 +21,8 @@ function generateDate() {
     return (generateDay + "." + generateMonth + "." + generateYear).toString();
 }
 
-function getRandomStudent() {
+
+function getRandomMail() {
     return ({
         id: getRandomID(0, 100),
         emailTo: getRandomEmail(),
@@ -36,67 +30,95 @@ function getRandomStudent() {
         date: generateDate()
     })
 }
+function getRandomID(min, max) {
+    let randomId = Math.random() * (max - min) + min;
+    randomId = Math.round(randomId);
+    return randomId;
+}
 
 function getEmailArray() {
     let emailList = [];
     const studentCount = Math.random() * (30 - 1) + 1;
     for (let i = 0; i < studentCount; i++) {
-        emailList.push(getRandomStudent())
+        emailList.push(getRandomMail())
     }
     return emailList
 }
 
-function getArrayByMonthAndYear() {
-    return getEmailArray().reduce(function(acc, obj) {
-        const [, month,year] = obj.date.split('.');
-        let key = `${month}.${year}`;
-        if (!acc.map.hasOwnProperty(key)) {
-            acc.map[key] = [];
-        }
-        acc.keySet.add(key);
-        acc.map[key].push(obj);
-        return acc;
-    }, { keySet: new Set(), map: {} })
+let result = sortedFetchData();
+
+
+ function fetchData() {
+    let emailArray = getEmailArray();
+    const getFormattedArr = (emArr) => {
+        return emArr.reduce(function (acc, obj) {
+            const [, month, year] = obj.date.split('.');
+            let key = `${month}.${year}`;
+            if (!acc.newMap.hasOwnProperty(key)) {
+                acc.newMap[key] = [];
+            }
+            acc.keySet.add(key);
+            acc.newMap[key].push(obj);
+            return acc;
+        }, {keySet: new Set(), newMap: {}});
+    };
+
+    return new Promise((resolve => setTimeout(() => resolve(getFormattedArr(emailArray)), 3000)));
+
+ }
+function getMapFromFetchData() {
+    fetchData().then(function (res) {
+        const {newMap} = res;
+        return newMap;
+    });
+}
+
+function sortedFetchData() {
+    fetchData().then(function (res) {
+        const {keySet} = res;
+        return result = sortByYearAndMonth(Array.from(keySet))
+    });
 }
 
 function sortByYearAndMonth(valuesToSort) {
-    return Array.from(valuesToSort)
+    return (valuesToSort)
         .map(item => item.split("."))
         .sort(( a, b) => ((Number(a[0]))  + Number((b[0]))) - (Number(a[1]) + Number(b[1])))
         .map(item => item.join("."));
 }
 
-function getObjectByCurrentMonthAndYear() {
-    let currentMonth = usersObject.sortedKeys[selectMonth];
-    let data = usersObject.map[currentMonth];
+/*function getObjectByCurrentMonthAndYear() {
 
+    let currentMonth = sortedFetchData()[selectMonth];
+    let data = getMapFromFetchData()[currentMonth];
     return {[currentMonth]: data};
-}
-console.log(getObjectByCurrentMonthAndYear());
+}*/
 
 
-function getNextMonth() {
 
-    if (selectMonth < usersObject.sortedKeys.length -1  ){
+
+/*function getNextMonth() {
+
+    if (selectMonth < usersObject.sortedKeys.length - 1) {
         selectMonth++;
         return getObjectByCurrentMonthAndYear()
     }
 
     else {
-        return"Haven't users yet"
+        return "Haven't users yet"
     }
 }
-
 function getPreviousMonth() {
     if (selectMonth > 0) {
         selectMonth--;
         return getObjectByCurrentMonthAndYear()
     }
     else {
-        return"Haven't users yet"
+        return "Haven't users yet"
     }
-}
-console.log(sortByYearAndMonth(usersObject.keySet));
+}*/
+
+/*console.log(sortByYearAndMonth(usersObject.keySet));
 console.log(getObjectByCurrentMonthAndYear(),'current')
 console.log(getNextMonth(),"next");
 console.log(getNextMonth());
@@ -108,9 +130,10 @@ console.log(getNextMonth());
 console.log(getNextMonth());
 console.log(getPreviousMonth(),'prev');
 console.log(getPreviousMonth(),'prev');
-console.log(getPreviousMonth(),'prev');
+console.log(getPreviousMonth(),'prev');*/
 
 
+/*
 document.getElementById("object").innerHTML = JSON.stringify(getObjectByCurrentMonthAndYear(), null, ' ');
 
 function rightClick() {
@@ -119,4 +142,4 @@ function rightClick() {
 
 function leftClick() {
     document.getElementById("object").innerHTML = JSON.stringify(getPreviousMonth(), null, ' ');
-}
+}*/
