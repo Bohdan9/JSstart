ac23a7560dea62f723b12371cc9d6bef0603c6e5
bohdan9 <protsbohdan9@gmail.com>
2019-05-31 01:45:59 +0300

    fix promise function

--- demo.js
+++ demo.js
@@ -1,4 +1,6 @@
 let selectMonth = 0;
+let emailsMap = null;
+let sortedMapKeys = null;
 
 
 function getRandomEmail() {
@@ -21,6 +23,11 @@ function generateDate() {
     return (generateDay + "." + generateMonth + "." + generateYear).toString();
 }
 
+function getRandomID(min, max) {
+    let randomId = Math.random() * (max - min) + min;
+    randomId = Math.round(randomId);
+    return randomId;
+}
 
 function getRandomMail() {
     return ({
@@ -31,18 +38,13 @@ function getRandomMail() {
     })
 }
 
-function getRandomID(min, max) {
-    let randomId = Math.random() * (max - min) + min;
-    randomId = Math.round(randomId);
-    return randomId;
-}
-
 function getEmailArray() {
     let emailList = [];
     const studentCount = Math.random() * (30 - 1) + 1;
     for (let i = 0; i < studentCount; i++) {
         emailList.push(getRandomMail())
     }
+
     return emailList
 }
 
@@ -53,7 +55,7 @@ let promiseObj = new Promise(function (res) {
 });
 
 function fetchData() {
-     return promiseObj.then(function (value) {
+    return this.promiseObj.then(function (value) {
         return value.reduce(function (acc, obj) {
             const [, month, year] = obj.date.split('.');
             let key = `${month}.${year}`;
@@ -67,81 +69,28 @@ function fetchData() {
     });
 }
 
-function getSortedKeys() {
-    fetchData().then(function (res) {
-        console.log(res);
-        const {keySet} = res;
-        return sortByYearAndMonth(Array.from(keySet))
-    }).then(console.log);
-}
-
 function sortByYearAndMonth(valuesToSort) {
-    return (valuesToSort)
+    return Array.from(valuesToSort)
         .map(item => item.split("."))
         .sort((a, b) => (Number(a[0])) - Number((b[0])))
         .sort((a, b) => Number((a[1])) - Number((b[1])))
         .map(item => item.join("."));
 }
-var map, keySet;
-fetchData().then(res => {
-    map = res.map;
-    keySet = res.keySet;
-}).then(() => getObjectByCurrentMonthAndYear(keySet, 0, map)).then(console.log)
-var sortedKeys = getSortedKeys;
 
-// let res = getSortedKeys();
-function getObjectByCurrentMonthAndYear(keys,selectMonth, map) {
-    let currentMonth = keySet[selectMonth];
-    let data = map[currentMonth];
-    console.log(currentMonth, 'currentMonth', data, 'data', map, 'map', keys, 'keys')
+function saveData(data) {
+    this.emailsMap = data.map;
+   // console.log(data)
+    console.log(this.selectMonth)
+    this.sortedMapKeys = this.sortByYearAndMonth(data.keySet);
+}
+
+function getObjectByCurrentMonthAndYear() {
+    let currentMonth = this.sortedMapKeys[selectMonth];
+    let data = this.emailsMap[currentMonth];
     return {[currentMonth]: data};
 }
 
-
-
-/*function getNextMonth() {
-
-    if (selectMonth < usersObject.sortedKeys.length - 1) {
-        selectMonth++;
-        return getObjectByCurrentMonthAndYear()
-    }
-
-    else {
-        return "Haven't users yet"
-    }
-}
-function getPreviousMonth() {
-    if (selectMonth > 0) {
-        selectMonth--;
-        return getObjectByCurrentMonthAndYear()
-    }
-    else {
-        return "Haven't users yet"
-    }
+/*function getNextMonthAndYear(){
+    selectMonth++;
 }*/
-
-/*console.log(sortByYearAndMonth(usersObject.keySet));
-console.log(getObjectByCurrentMonthAndYear(),'current')
-console.log(getNextMonth(),"next");
-console.log(getNextMonth());
-console.log(getNextMonth());
-console.log(getNextMonth());
-console.log(getNextMonth());
-console.log(getNextMonth());
-console.log(getNextMonth());
-console.log(getNextMonth());
-console.log(getPreviousMonth(),'prev');
-console.log(getPreviousMonth(),'prev');
-console.log(getPreviousMonth(),'prev');*/
-
-
-/*
-document.getElementById("object").innerHTML = JSON.stringify(getObjectByCurrentMonthAndYear(), null, ' ');
-
-function rightClick() {
-    document.getElementById("object").innerHTML = JSON.stringify(getNextMonth(), null, ' ');
-}
-
-function leftClick() {
-    document.getElementById("object").innerHTML = JSON.stringify(getPreviousMonth(), null, ' ');
-}*/
+fetchData().then(this.saveData.bind(this)).then(this.getObjectByCurrentMonthAndYear);
